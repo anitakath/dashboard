230c89441dc903bf181679f413761f43d63b3dc7
anitakath <wagner.annekathrin@gmx.de>
2024-10-28 11:35:46 +0100

    implementing jest testing

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: ["next/babel"],
+};
+
+
+/*
+   // babel.config.js
+   module.exports = {
+     presets: [
+       '@babel/preset-env',
+       '@babel/preset-react'
+     ]
+   };
+*/
