0b89219fc2f55447ac7f3d64efbe366d18646b76
anitakath <wagner.annekathrin@gmx.de>
2024-11-08 11:10:52 +0100

    searched entries are now sorted per year and month

--- components/UI/ResultsBar.js
+++ components/UI/ResultsBar.js
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import styles from "./ResultsBar.module.css";
 
-import { getMonth } from "@/custom-hooks/formatDate";
-
-const ResultsBar = ({ filteredSearchedEntries }) => {
+import { getMonth } from "@/custom-hooks/formatDate";const ResultsBar = ({ filteredSearchedEntries }) => {
   // Check whether there are filtered entries
   if (!filteredSearchedEntries || filteredSearchedEntries.length === 0) {
     return (
@@ -13,32 +11,44 @@ const ResultsBar = ({ filteredSearchedEntries }) => {
     );
   }
 
+  const groupByMonthYear = (entries) => {
+    return entries.reduce((acc, entry) => {
+      const date = new Date(entry.created_at);
+      const monthName = getMonth(entry.created_at);
+      const year = date.getFullYear();
+      const monthYearKey = `${year}, ${monthName}`;
+      if (!acc[monthYearKey]) {
+        acc[monthYearKey] = [];
+      }
+      acc[monthYearKey].push(entry);
+      return acc;
+    }, {});
+  };
 
-
-  const test = getMonth(filteredSearchedEntries[0])
-
-
- const groupByMonthYear = (entries) => {
-   return entries.reduce((acc, entry) => {
-     const date = new Date(entry.created_at);
-     const monthName = getMonth(entry.created_at); 
-     const year = date.getFullYear(); 
-
-     const monthYearKey = `${year}, ${monthName}`; 
-
-     if (!acc[monthYearKey]) {
-       acc[monthYearKey] = [];
-     }
-     acc[monthYearKey].push(entry);
-     return acc;
-   }, {});
- };
   const groupedEntries = groupByMonthYear(filteredSearchedEntries);
+
+  // Sort the grouped entries by year (descending) and month (descending)
+  const sortedGroupedEntries = Object.entries(groupedEntries).sort((a, b) => {
+    const [yearA, monthA] = a[0].split(", ");
+    const [yearB, monthB] = b[0].split(", ");
+
+    // Compare years first (descending)
+    if (yearA !== yearB) {
+      return yearB - yearA; // Sort descending by year
+    }
+
+    // If years are the same, compare months (descending)
+    return (
+      new Date(Date.parse(monthA + " 1, " + yearA)) -
+      new Date(Date.parse(monthB + " 1, " + yearB))
+    );
+  });
+
 
   return (
     <div className={styles.resultsContainer}>
-      {Object.entries(groupedEntries).map(([monthYear, entries]) => (
-        <div key={monthYear} className=" flex flex-col">
+      {sortedGroupedEntries.map(([monthYear, entries]) => (
+        <div key={monthYear} className="flex flex-col">
           <h2 className={styles.monthTitle}>{monthYear}</h2>
           {entries.map((entry) => (
             <Link
